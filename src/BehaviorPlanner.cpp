1cdc31ff7e3fc3673940b999a390c65bbddf2088
Christoph Stephan <chm.stephan@outlook.com>
2018-12-11 01:00:53 +0100

    implement behavior: follow vehicle in front of us

--- src/BehaviorPlanner.cpp
+++ src/BehaviorPlanner.cpp
@@ -11,5 +11,19 @@ Behavior BehaviorPlanner::PlanNextBehavior(const VehicleState& car, const std::v
     behavior.max_speed = this->max_speed;
     behavior.min_distance = this->min_distance_travel_time * this->max_speed;
     behavior.vehicle_id = -1;
+    // find nearest vehicle in same lane in front of us
+    auto nearest = this->map.max_s;
+    for (auto vehicle : sensor_fusion) {
+        if (this->map.GetLaneFrom(vehicle.frenet) == behavior.lane) {
+            auto dist = vehicle.frenet.s - car.frenet.s;
+            if (dist <= 0_m) {
+                dist += this->map.max_s;
+            }
+            if (dist < nearest) {
+                nearest = dist;
+                behavior.vehicle_id = vehicle.id;
+            }
+        }
+    }
     return behavior;
 }
