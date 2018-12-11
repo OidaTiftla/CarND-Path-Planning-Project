b6b6059a2101969c0321fb82c36eca2e43782062
Christoph Stephan <chm.stephan@outlook.com>
2018-12-12 00:30:35 +0100

    create logging function

---
+++ src/log.h
@@ -0,0 +1,25 @@
+//
+// Created by chmst on 10/11/2016.
+//
+
+#ifndef LOG_H
+#define LOG_H
+
+#include <iostream>
+#include <sstream>
+
+
+#define LOG_LEVEL 0
+
+static std::stringstream nullstream;
+
+inline std::ostream& log(int level) {
+    if (level > LOG_LEVEL) {
+        nullstream.seekp(0);
+        return nullstream;
+    } else {
+        return std::cout;
+    }
+}
+
+#endif //LOG_H
