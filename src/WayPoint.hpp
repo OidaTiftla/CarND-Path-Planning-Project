1deb8715ba52611a96ca21f221490668031c6730
Christoph Stephan <chm.stephan@outlook.com>
2019-01-14 00:03:32 +0100

    const parameters

--- src/WayPoint.hpp
+++ src/WayPoint.hpp
@@ -12,7 +12,7 @@ public:
     const FrenetCoordinate frenet;
     const LocalCartesianCoordinate normalized_normal_vector;
 
-    WayPoint(GlobalCartesianCoordinate cartesian, FrenetCoordinate frenet, LocalCartesianCoordinate normalized_normal_vector) : cartesian(cartesian), frenet(frenet), normalized_normal_vector(normalized_normal_vector) {}
+    WayPoint(const GlobalCartesianCoordinate cartesian, const FrenetCoordinate frenet, const LocalCartesianCoordinate normalized_normal_vector) : cartesian(cartesian), frenet(frenet), normalized_normal_vector(normalized_normal_vector) {}
 };
 
 #endif //WAY_POINT_HPP
